428830a53e380ff6436319909813608dfb1474ca
otternq <otternq@gmail.com>
2013-04-25 06:27:07 +0000

    working prolog requests from django. Does not consult the family file correctly

--- api/tree/views.py
+++ api/tree/views.py
@@ -1,13 +1,26 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+
+from pyswip import Prolog
+
 # Create your views here.
 
 
 def index(request, table):
 
+    prolog = Prolog()
+
+    prolog.consult('family')
+
+    prolog.assertz("father(michael,john)")
+    prolog.assertz("father(michael,gina)")
+    
+    father = list(prolog.query("father(Y, X)"))
+
     #return HttpResponse(response, mimetype="application/xml")
     t = loader.get_template('index/select.html')
-    c = RequestContext(request, {'table': table})
+    c = RequestContext(request, {'table': table, 'father': father})
+
 
     return HttpResponse(
         t.render(c), 
